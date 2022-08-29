2e11f4751da5bd4dba4f806962bc75a9fc2c9384
Simon Willison <swillison@gmail.com>
2022-08-29 15:05:51 -0700

    Initial implemenatation plus test, refs #1

--- datasette_sitemap/__init__.py
+++ datasette_sitemap/__init__.py
@@ -1,1 +1,69 @@
-from datasette import hookimpl
+from datasette import hookimpl, Response
+
+ROBOTS_TXT = """
+Sitemap: https://www.niche-museums.com/sitemap.xml
+""".strip()
+
+
+class SitemapConfig:
+    def __init__(self, sql, database, base_url):
+        self.sql = sql
+        self.database = database
+        self.base_url = base_url
+
+
+def _sitemap_config(datasette):
+    plugin_config = datasette.plugin_config("datasette-sitemap") or {}
+    if plugin_config.get("sql"):
+        return SitemapConfig(
+            plugin_config.get("sql"),
+            plugin_config.get("database"),
+            plugin_config.get("base_url"),
+        )
+    return None
+
+
+@hookimpl
+def register_routes(datasette):
+    if _sitemap_config(datasette):
+        return [
+            ("^/robots.txt$", robots_txt),
+            ("^/sitemap.xml$", sitemap_xml),
+        ]
+
+
+def _make_url_maker(datasette):
+    config = _sitemap_config(datasette)
+    if config.base_url:
+        return lambda _, path: config.base_url + path
+    else:
+        return lambda request, path: datasette.absolute_url(request, path)
+
+
+def robots_txt(datasette, request):
+    url = _make_url_maker(datasette)
+    return Response.text("Sitemap: {}".format(url(datasette, request, "/sitemap.xml")))
+
+
+class SitemapError(Exception):
+    pass
+
+
+async def sitemap_xml(datasette, request):
+    config = _sitemap_config(datasette)
+    url = _make_url_maker(datasette)
+    content = [
+        '<?xml version="1.0" encoding="UTF-8"?>',
+        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    ]
+    db = datasette.get_database(config.database)
+    for row in await db.execute(config.sql + " limit 50000"):
+        try:
+            path = row["path"]
+        except IndexError:
+            raise SitemapError("SQL query must return a path column")
+        if not path.startswith("/"):
+            raise SitemapError("Path '{}' must start with /".format(path))
+        content.append("<url><loc>{}</loc></url>".format(url(request, row["path"])))
+    content.append("</urlset>")
+    return Response("\n".join(content), 200, content_type="application/xml")
--- tests/test_sitemap.py
+++ tests/test_sitemap.py
@@ -1,11 +1,80 @@
 from datasette.app import Datasette
+from xml.etree import ElementTree as ET
 import pytest
 
 
 @pytest.mark.asyncio
-async def test_plugin_is_installed():
-    datasette = Datasette(memory=True)
-    response = await datasette.client.get("/-/plugins.json")
-    assert response.status_code == 200
-    installed_plugins = {p["name"] for p in response.json()}
-    assert "datasette-sitemap" in installed_plugins
+@pytest.mark.parametrize(
+    "sql,database,expected_paths,expected_error",
+    (
+        (
+            "select 'nopath' as nopath",
+            None,
+            None,
+            "SQL query must return a path column",
+        ),
+        ("select 'path' as path", None, None, "Path &#39;path&#39; must start with /"),
+        ("select '/' as path", None, ["http://localhost/"], None),
+        ("select path from urls", "two", ["http://localhost/3"], None),
+        (
+            "select '/' as path union select '/2' as path",
+            None,
+            ["http://localhost/", "http://localhost/2"],
+            None,
+        ),
+        # This returns 50001 rows, but we should only get 50000
+        (
+            """
+        with recursive counter(x) as (
+        select 0
+            union
+        select x + 1 from counter
+        ),
+        paths as (
+            select '/' || x as path from counter limit 50001
+        ) select path from paths
+            """,
+            None,
+            ["http://localhost/{}".format(i) for i in range(50000)],
+            None,
+        ),
+    ),
+)
+async def test_datasette_sitemap(sql, database, expected_paths, expected_error):
+    datasette = Datasette(
+        memory=True,
+        metadata={
+            "plugins": {
+                "datasette-sitemap": {
+                    "sql": sql,
+                    "database": database,
+                }
+            }
+        },
+    )
+    if database == "two":
+        two = datasette.add_memory_database("two")
+        await two.execute_write_script(
+            """
+            create table if not exists urls (path text);
+            insert into urls values ('/3');
+        """
+        )
+    response = await datasette.client.get("/sitemap.xml")
+    if expected_error:
+        assert response.status_code == 500
+        assert expected_error in response.text
+    else:
+        assert response.status_code == 200
+        # It should be XML
+        assert response.headers["content-type"] == "application/xml"
+        # Parse the XML
+        tree = ET.fromstring(response.text)
+        # Pull out the <urlset> <url> <loc> elements
+        paths = [
+            url.text
+            for url in tree.findall(
+                "{http://www.sitemaps.org/schemas/sitemap/0.9}url/{http://www.sitemaps.org/schemas/sitemap/0.9}loc"
+            )
+        ]
+        assert paths == expected_paths
